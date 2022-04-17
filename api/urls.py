4ea88d2c852d386b75e09c0f044391e1560a0fc2
bl4ckh401 <pavkiptoo@gmail.com>
2022-04-17 21:49:08 +0300

    show images

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import CreateProjectView, GetCSRFToken, ProjectView, SendMessage, ViewMessages
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('create-project/', CreateProjectView.as_view(), name='create_project'),
@@ -7,4 +9,4 @@ urlpatterns = [
     path('getcsrf/', GetCSRFToken.as_view()),
     path('chats/', ViewMessages.as_view()),
     path('send-chats/', SendMessage.as_view()),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from views import index
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index),
     path('work/', index),
     path('contact', index),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
