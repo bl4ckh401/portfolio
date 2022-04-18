cb550bdf4d159217f54bad22034ed141644f3757
bl4ckh401 <pavkiptoo@gmail.com>
2022-04-18 19:13:16 +0300

    fix image display

--- src/Components/Homebottom.js
+++ src/Components/Homebottom.js
@@ -4,49 +4,52 @@ function Homebottom() {
     const [projects, setProjects] = useState([])
 
     const onLoad = () => {
-        const requestOptions = { 
-            method:"GET",
-            headers: { 'Content-Type': "application/json"},
-            }
-            fetch('/api/work/', requestOptions)
+        const requestOptions = {
+            method: "GET",
+            headers: { 'Content-Type': "application/json" },
+        }
+        fetch('/api/work/', requestOptions)
             .then(response => response.json())
             .then((data) => {
                 setProjects(data)
-                console.log(data)}
-            )}
+                console.log(data)
+                console.log(data.cover_image.url)
+            }
+            )
+    }
 
     useEffect(() => {
-        onLoad()    
+        onLoad()
     }, []);
-     
-  return <div className='homebottom'>
-      <div className='card_holder'>
+
+    return <div className='homebottom'>
+        <div className='card_holder'>
             {
                 projects.map((project) => {
-                    return(
-                    <div className="catalogue_card" key={project.id}>
-                        <div className="catalogue_org">
-                            <div className="catalogue_main">
-                            <div className="item_desc">
-                                <div className="catalogue_img">
-                                    <img className="catalogue_img" src={project.cover_image} alt="alternative project image" ></img>
-                                </div>
-                                <div className="item_div">
-                                    <h3>Title : <span className="item_name">{project.project_title}</span></h3>
-                                </div>
-                                <div className="item_div">
-                                    <h3>Languages : <span className="item_name">{project.languages}</span></h3>
-                                </div>
+                    return (
+                        <div className="catalogue_card" key={project.id}>
+                            <div className="catalogue_org">
+                                <div className="catalogue_main">
+                                    <div className="item_desc">
+                                        <div className="catalogue_img">
+                                            <img className="catalogue_img" src={project.cover_image.url} alt="alternative project image" />
+                                        </div>
+                                        <div className="item_div">
+                                            <h3>Title : <span className="item_name">{project.project_title}</span></h3>
+                                        </div>
+                                        <div className="item_div">
+                                            <h3>Languages : <span className="item_name">{project.languages}</span></h3>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                )
-            })
-        }
+                    )
+                })
+            }
 
-      </div>
-</div>;
+        </div>
+    </div>;
 }
 
 export default Homebottom;
